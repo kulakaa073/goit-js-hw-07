a91a73191400808ad10b01d92e1e6a378161f37f
Shade <kulakaa073@gmail.com>
2025-02-17 22:21:10 +0200

    Added task 6 and css for it

--- js/task-6.js
+++ js/task-6.js
@@ -4,12 +4,37 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-const boxesContainer = document.querySelector("#boxes");
 const controls = document.querySelector("#controls");
-console.log(controls);
-console.log(boxesContainer);
-
 const controlsInput = controls.querySelector("input");
 const controlsCreateButton = controls.querySelector('[data-create]');
-console.log(controlsInput);
-console.log(controlsCreateButton);
+const controlsDestroyButton = controls.querySelector('[data-destroy]');
+
+controlsCreateButton.addEventListener("click", () => { 
+  const numberOfBoxes = controlsInput.value;
+  if (numberOfBoxes < 1 || numberOfBoxes > 100) {
+    alert("Number of boxes should be between 1 and 100");
+    return;
+  }
+  const boxesContainer = document.querySelector("#boxes");
+  const startingSize = 30;
+  const sizeStep = 10;
+  const tempBoxesContainer = document.createElement("div");
+  tempBoxesContainer.setAttribute("id", "boxes");
+  tempBoxesContainer.style.display = "flex";
+  tempBoxesContainer.style.flexDirection = "row";
+  tempBoxesContainer.style.flexWrap = "wrap";
+  tempBoxesContainer.style.gap = "44px 44px";
+  tempBoxesContainer.style.alignContent = "baseline";
+  for (let i = 0; i < numberOfBoxes; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${startingSize + sizeStep * i}px`;
+    box.style.height = `${startingSize + sizeStep * i}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    tempBoxesContainer.append(box);
+  }
+  boxesContainer.replaceWith(tempBoxesContainer);
+});
+
+controlsDestroyButton.addEventListener("click", () => {
+  document.querySelector("#boxes").innerHTML = "";
+});
